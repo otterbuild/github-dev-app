5e238a3927eadd0123d6382064d7382c053ec0c0
Jan David <jdno@jdno.dev>
2024-06-20 00:39:58 +0200

    Bootstrap command to register a GitHub App (#6)
    The initial structure for commands has been created, using the
    `register` command as an example. The command will allow users to
    register a new GitHub App from a manifest file. At this point, the
    command does nothing more than read a manifest file, parse it, and print
    it as JSON to the terminal.

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,33 @@
 #![warn(clippy::missing_docs_in_private_items)]
 #![warn(missing_docs)]
 
+use anyhow::Error;
+use async_trait::async_trait;
 use clap::Parser;
 
-use crate::cli::Args;
+use crate::cli::{Args, Command};
+use crate::register::RegisterCommand;
 
 mod cli;
 mod manifest;
+mod register;
 
-fn main() {
-    let _args = Args::parse();
+/// Execute a command
+///
+/// This trait must be implemented by the subcommands of the command-line tool. It provides a single
+/// method, `execute`, that will be called by the main function to run the command.
+#[async_trait]
+trait Execute {
+    async fn execute(&self, global_args: &Args) -> Result<(), Error>;
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let global_args = Args::parse();
+
+    let command: Box<dyn Execute> = match global_args.command() {
+        Command::Register(args) => Box::new(RegisterCommand::new(args)),
+    };
+
+    command.execute(&global_args).await
 }
