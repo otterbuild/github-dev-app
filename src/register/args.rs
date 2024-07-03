ff05d71efd9ffb3a5cf87954b7f509cd4e6f958e
Jan David <jdno@jdno.dev>
2024-07-03 18:31:02 +0200

    Overwrite redirect URL in manifest
    When reading and parsing a manifest, the `redirect_url` field is now
    overwritten with the URL of the local web server. After registering a
    new GitHub App, GitHub will redirect the user to this URL to finish the
    installation.

--- src/register/args.rs
+++ src/register/args.rs
@@ -4,16 +4,20 @@ use std::path::PathBuf;
 
 use clap::Parser;
 use getset::Getters;
+use typed_builder::TypedBuilder;
 
 /// Command-line arguments for the `register` subcommand
 ///
 /// The `register` subcommand is used to register a new GitHub App from a manifest file. The command
 /// requires the path to the manifest file as an argument, and optionally accepts other arguments to
 /// customize the manifest.
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default, Parser, Getters)]
+#[derive(
+    Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default, Parser, Getters, TypedBuilder,
+)]
 pub struct RegisterArgs {
     /// The path to the manifest file
     #[arg()]
+    #[builder(setter(into))]
     #[getset(get = "pub")]
     manifest: PathBuf,
 }
